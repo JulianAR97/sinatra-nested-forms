61b49f977a0ebe1f1b1a1c455cb1887a35210f5f
Julian <julianrosenthal@yahoo.com>
2020-12-22 11:07:54 -0800

    commit tree c8ffbaea6a2f146f6ecf6462076201751a93e8ef

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+    @@all = []
+    
+    def initialize(details)
+        @name = details["name"]
+        @weight = details["weight"]
+        @height = details["height"]
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+    attr_accessor :name, :type, :booty
+    @@all = []
+    
+    def initialize(details)
+        @name = details["name"]
+        @type = details["type"]
+        @booty = details["booty"]
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all = []
+    end
 end
